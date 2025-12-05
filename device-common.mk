be1ec4443a48f7e8f0e0a551daf4a14d76c629f7
Michael Bestas <mkbestas@lineageos.org>
2025-12-05 06:51:24 +0200

    redbull: Drop unused PixelLogger
    Change-Id: I77b3947b06ea273fd5c692bbb0cf1e656b5dd788

--- device-common.mk
+++ device-common.mk
@@ -782,9 +782,6 @@ PRODUCT_PACKAGES += \
 
 # mm_event
 -include hardware/google/pixel/mm/device.mk
-
-# Pixel Logger
-include hardware/google/pixel/PixelLogger/PixelLogger.mk
 #################################################################################
 
 # Update soong config namespace
